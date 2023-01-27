997b9fa54ebc47bcef5f489b16ec6a6baebceb54
Saurabh Dixit <86460997+Saurabhdixit93@users.noreply.github.com>
2023-01-27 21:25:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,15 @@
-// // Get the form element
-// var form = document.getElementById("myForm");
-
-// // Add an event listener to the form to listen for the submit event
-// form.addEventListener("submit", function(event) {
-//   // Get the name, email, and message elements
-//   var name = document.getElementById("name").value;
-//   var email = document.getElementById("email").value;
-//   var message = document.getElementById("message").value;
-
-//   // Check if the name field is empty
-//   if (name === "") {
-//     alert("Please enter your name");
-//     console.log("11");
-//     event.preventDefault();
-//   }
-
-//   // Check if the email field is empty or is not a valid email address
-//   if (email === "" || !validateEmail(email)) {
-//     alert("Please enter a valid email address");
-//     event.preventDefault();
-//   }
-
-//   // Check if the message field is empty
-//   if (message === "") {
-//     alert("Please enter a message");
-//     event.preventDefault();
-//   }
-// });
-
-// function validateEmail(email) {
-//   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//   return re.test(String(email).toLowerCase());
-// }
-
-// // Get all navigation links
-// var navLinks = document.querySelectorAll("nav a");
-
-// // Add a click event listener to each link
-// navLinks.forEach(function(link) {
-//   link.addEventListener("click", function(event) {
-//     event.preventDefault();
-
-//     // Get the target element to scroll to
-//     var target = document.querySelector(link.getAttribute("href"));
-
-//     // Calculate the distance from the top of the target element to the top of the document
-//     var targetPos = target.getBoundingClientRect().top;
-
-//     // Scroll to the target element
-//     window.scrollBy({
-//       top: targetPos,
-//       behavior: "smooth"
-//     });
-//   });
-// });
-
-
-
-
 
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .flex .navbar');
+var email = document.getElementById("email").value
+
+// Check Email Valid Function
+
+function validateEmail(email) {
+     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+     return re.test(String(email).toLowerCase());
+   }
+
 
 menu.onclick = () => {
    menu.classList.toggle('fa-times');
