13c0f1a37bc64ba87e3bcf9c7ccbb1454298ce27
Saurabh Dixit <86460997+Saurabhdixit93@users.noreply.github.com>
2023-01-30 14:27:08 +0530

    Saurabh Dixit 30

---
+++ about.js
@@ -0,0 +1,49 @@
+// JavaScript
+
+// Get the preloader element
+// document.addEventListener("DOMContentLoaded", function() {
+//     var preloader = document.getElementById("preloader");
+//     preloader.style.display = "none";
+//   });
+//   document.addEventListener("DOMContentLoaded", function() {
+//     var preloader = document.getElementById("preloader");
+//     preloader.classList.add("hide");
+//   });
+  document.addEventListener("DOMContentLoaded", function() {
+    var preloader = document.getElementById("preloader");
+    preloader.style.display = "flex";
+    setTimeout(function() {
+      preloader.style.display = "none";
+    }, 1000);
+  });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var preloader = document.getElementById("preloader");
+    preloader.classList.add("show");
+    setTimeout(function() {
+      preloader.classList.remove("show");
+      preloader.classList.add("hide");
+    }, 1000);
+  });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var preloaderText = document.querySelector("#preloader .icon-text");
+    setInterval(function() {
+      preloaderText.classList.toggle("blink");
+    }, 1000);
+  });
+  
+//   Show More Feature
+
+const showMoreBtn = document.querySelector('.show-more-btn');
+const moreText = document.querySelector('.more-text');
+
+showMoreBtn.addEventListener('click', function() {
+  if(moreText.style.display === 'none'){
+    moreText.style.display = 'block';
+    this.innerHTML = 'Show Less';
+  }else{
+    moreText.style.display = 'none';
+    this.innerHTML = 'Show More';
+  }
+});
--- script.js
+++ script.js
@@ -1,4 +1,103 @@
+// // Get the form element
+// var form = document.getElementById("myForm");
 
+// // Add an event listener to the form to listen for the submit event
+// form.addEventListener("submit", function(event) {
+//   // Get the name, email, and message elements
+//   var name = document.getElementById("name").value;
+//   var email = document.getElementById("email").value;
+//   var message = document.getElementById("message").value;
+
+//   // Check if the name field is empty
+//   if (name === "") {
+//     alert("Please enter your name");
+//     console.log("11");
+//     event.preventDefault();
+//   }
+
+//   // Check if the email field is empty or is not a valid email address
+//   if (email === "" || !validateEmail(email)) {
+//     alert("Please enter a valid email address");
+//     event.preventDefault();
+//   }
+
+//   // Check if the message field is empty
+//   if (message === "") {
+//     alert("Please enter a message");
+//     event.preventDefault();
+//   }
+// });
+
+// function validateEmail(email) {
+//   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//   return re.test(String(email).toLowerCase());
+// }
+
+// // Get all navigation links
+// var navLinks = document.querySelectorAll("nav a");
+
+// // Add a click event listener to each link
+// navLinks.forEach(function(link) {
+//   link.addEventListener("click", function(event) {
+//     event.preventDefault();
+
+//     // Get the target element to scroll to
+//     var target = document.querySelector(link.getAttribute("href"));
+
+//     // Calculate the distance from the top of the target element to the top of the document
+//     var targetPos = target.getBoundingClientRect().top;
+
+//     // Scroll to the target element
+//     window.scrollBy({
+//       top: targetPos,
+//       behavior: "smooth"
+//     });
+//   });
+// });
+
+
+
+// var scrollContainer = document.querySelector('.scroll-container');
+// var scrollPercentage = document.querySelector('.scroll-percentage');
+// var scrollPercent = 0;
+// document.addEventListener('scroll', function() {
+//   scrollPercent = Math.floor((document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100);
+//   scrollPercentage.innerText = scrollPercent + '%';
+//   scrollPercentage.style.width = scrollPercent + "%";
+  
+//   if (scrollPercent >= 25 && scrollPercent < 50) {
+//     scrollPercentage.classList.add("reached-25");
+//   } else if (scrollPercent >= 50 && scrollPercent < 75) {
+//     scrollPercentage.classList.add("reached-50");
+//   } else if (scrollPercent >= 75 && scrollPercent < 100) {
+//     scrollPercentage.classList.add("reached-75");
+//   } else if (scrollPercent >= 100) {
+//     scrollPercentage.classList.add("reached-100");
+//   }
+// });
+
+
+// Pre Loader
+
+// const fade =()=>{
+//    const loader = document.querySelector('.loader');
+//    loader.classList.add('fade');
+// };
+// window.addEventListener('load',fade);
+
+// Get the preloader element
+$(document).ready(function() {
+  // Show preloader
+  $("#preloader").fadeIn();
+
+  // Hide preloader after all assets are loaded
+  $(window).on("load", function() {
+    $("#preloader").fadeOut();
+  });
+});
+
+
+ 
 // Add an event listener to the form to listen for the submit event
 
 var form = document.getElementById("myform");
@@ -56,10 +155,22 @@ var swiper = new Swiper('.swiper-container', {
       prevEl: '.swiper-button-prev',
    }
 });
-
-
 // For Scrolling View Page
+
+// window.addEventListener("scroll", function(){
+//   var scrollPercent = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100;
+//   document.getElementById("scroll-bar").style.width = scrollPercent + "%";
+// });
+
+const progressBar = document.getElementById("scroll-bar");
+const pointer = document.querySelector(".pointer");
+
 window.addEventListener("scroll", function(){
-   var scrollPercent = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100;
-   document.getElementById("scroll-bar").style.width = scrollPercent + "%";
- });
+  var scrollPercent = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100;
+  progressBar.style.width = scrollPercent + "%";
+  pointer.style.left = scrollPercent + "%";
+});
+
+
+
+
